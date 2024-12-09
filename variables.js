0593b8712284f71eb3079d668551a9d0fb2a9445
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2024-12-09 18:41:41 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,44 @@
+//1. Declare a variable to store your name and log it to the console.
+let Name="Abdul samad jamali";
+console.log(Name)
+//2. Create two variables to store your age and a friend’s age, then log the sum.
+let my_age=20;
+let friend_age=21;
+let result=my_age+friend_age
+console.log("The sum of ages is ",result)
+// 3. Declare a constant to store the value of Pi (3.14159).
+const pi=3.14159;
+console.log("The value of Pi is ",pi)
+
+// 4. Store your favorite color in a variable and display it in a sentence.
+let favorite_color="black";
+console.log(`my favorite color is ${favorite_color}`)
+// 5. Swap the values of two variables without creating a third variable.
+var a=2
+var b=3
+var a=a+b
+var b=a-b
+var a=a-b
+console.log(`The value of A is ${a} and the value of B is ${b}`) 
+// 6. Declare a variable with no initial value, then assign a number to it.
+ let my_number;
+ my_number=10;
+ console.log(my_number)
+// 7. Create a variable that holds the result of multiplying two numbers.
+var a=7;
+var b=8;
+var Result=a*b;
+console.log(` The result of multiplying two numb is ${Result}`);
+// 8. Store the result of dividing two numbers and log whether the result is an integer.
+ var a=10;
+ var b=3;
+ var Result=a/b;
+ console.log(`The result of dividing two numb is ${Result} and it is ${Number.isInteger(Result)}`)
+// 9. Declare variables for first name, last name, and age. Log a sentence combining them.
+var first_name="abdul Samad"
+var last_name="Jamali"
+console.log(` My First name  ${first_name} and the last name is ${last_name}`)
+// 10. Declare a Boolean variable to indicate whether today is sunny. Log the value.
+var Boolean=true
+console.log(`today is  sunny day ${Boolean} \n `)
+
