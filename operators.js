c6cf10e7523fb238590bb2c5556fe3cc0a37fda4
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2024-12-10 21:31:02 +0500

    Add files via upload
    4th commit

---
+++ operators.js
@@ -0,0 +1,57 @@
+console.log("********************************")
+console.log("Secound Portion ")
+console.log("********************************")
+
+
+
+// 1. Use the addition operator to add two numbers and display the result.
+var x = 7;
+var y = 8;
+var Resu = x + y;
+console.log(Resu); 
+
+
+// 2. Use the modulus operator to check if a number is even or odd.
+var a = 5; // Declare the variable
+let Res = (a % 2 == 0) ? "Even" : "Odd"; 
+console.log(Res); 
+
+
+// 3. Write a program to compare two numbers and log which is larger.
+var i=20;
+var j=30;
+var larger=(i>j)?`i is greater b/c value of i is ${i} and j is  ${j}`:`j is greater b/c j= ${j} and i = ${i}`
+console.log(larger)
+
+// 4. Use the increment operator to increase a variable by 1.
+var k=20
+k++
+console.log(k)
+// 5. Use the decrement operator to decrease a variable by 1.
+var k=99
+k--
+console.log(k)
+// 6. Write a program to calculate the area of a rectangle (length × width).
+let length=33
+let width=88
+let Results=length*width
+console.log(Results)
+// 7. Use the += operator to add 10 to a variable.
+var p=66
+p+=10
+console.log(p)
+// 8. Write a program to calculate the simple interest (P × R × T) / 100.
+var principal_val=100
+var rate_of_intrest=10
+var time_period=10
+let simple_intrest=(principal_val*rate_of_intrest*time_period)/100
+console.log("The simple intrest on initial value is ",simple_intrest)
+// 9. Create a program to find the remainder when dividing 25 by 4.
+var z=20
+var reminder=(z/4)
+console.log(z)
+// 10. Check whether two strings are equal using the equality operator.
+let string1="abdul samad jamali"
+let string2="abdul samad jamali"
+let resultt=string1==string2
+console.log(resultt)
